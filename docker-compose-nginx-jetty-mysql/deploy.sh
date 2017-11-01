6a03d566802609d279386780ad12e4d97975a98d
Christos Tranoris <tranoris@gmail.com>
2017-11-01 17:08:01 +0200

    fix

--- docker-compose-nginx-jetty-mysql/deploy.sh
+++ docker-compose-nginx-jetty-mysql/deploy.sh
@@ -6,14 +6,14 @@ cd $dirlocation
 echo "Build eu.5ginFIRE.riftioyangschema2java"
 if [ ! -d eu.5ginFIRE.riftioyangschema2java ]; then
 	git clone https://github.com/5GinFIRE/eu.5ginFIRE.riftioyangschema2java.git
-	cd eu.5ginFIRE.riftioyangschema2java
+	cd eu.5ginFIRE.riftioyangschema2java/
 else
-	cd eu.5ginFIRE.riftioyangschema2java
+	cd eu.5ginFIRE.riftioyangschema2java/
 	git pull
 fi
 
 cd eu.5ginFIRE.riftioyangschema2java
-sudo docker run -it --rm -v "/home/ubuntu/.m2":/root/.m2 -v "$(pwd)":/opt/maven -w /opt/maven maven:3.5.2-jdk-8 mvn clean install
+#sudo docker run -it --rm -v "/home/ubuntu/.m2":/root/.m2 -v "$(pwd)":/opt/maven -w /opt/maven maven:3.5.2-jdk-8 mvn clean install
 
 
 echo "Build nfv-requirements-extractor"
@@ -26,12 +26,12 @@ else
 	git pull
 fi
 
-sudo docker run -it --rm -v "/home/ubuntu/.m2":/root/.m2 -v "$(pwd)":/opt/maven -w /opt/maven maven:3.5.2-jdk-8 mvn clean install
+#sudo docker run -it --rm -v "/home/ubuntu/.m2":/root/.m2 -v "$(pwd)":/opt/maven -w /opt/maven maven:3.5.2-jdk-8 mvn clean install
 
 echo "Build eu.5ginfire.portal.api"
 cd $dirlocation
 cd eu.5ginfire.portal.api
-sudo docker run -it --rm -v "/home/ubuntu/.m2":/root/.m2 -v "$(pwd)":/opt/maven -w /opt/maven maven:3.5.2-jdk-8 mvn clean install
+#sudo docker run -it --rm -v "/home/ubuntu/.m2":/root/.m2 -v "$(pwd)":/opt/maven -w /opt/maven maven:3.5.2-jdk-8 mvn clean install
 cp target/eu.5ginfire.portal.api-0.0.1-SNAPSHOT.war docker-compose-nginx-jetty-mysql/jetty/wars/
 
 echo "Updating eu.5ginfire.portal.web"
@@ -39,7 +39,7 @@ cd $dirlocation
 cd eu.5ginfire.portal.api/docker-compose-nginx-jetty-mysql/nginx
 if [ ! -d eu.5ginfire.portal.web]; then
         git clone https://github.com/5GinFIRE/eu.5ginfire.portal.web.git
-	cd eu.5ginfire.portal.web
+		cd eu.5ginfire.portal.web
 else
         cd eu.5ginfire.portal.web
         git pull

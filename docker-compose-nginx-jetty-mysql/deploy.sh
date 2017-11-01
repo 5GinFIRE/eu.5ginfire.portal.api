7292df5b060d2002320093353ac213ad00259ca3
Christos Tranoris <tranoris@gmail.com>
2017-11-01 10:48:35 +0200

    fixing deploy

--- docker-compose-nginx-jetty-mysql/deploy.sh
+++ docker-compose-nginx-jetty-mysql/deploy.sh
@@ -3,5 +3,15 @@
 dirlocation=`pwd`/../..
 echo "We're working with $dirlocation"
 cd $dirlocation
+rm eu.5ginFIRE.riftioyangschema2java.git
+rm nfv-requirements-extractor.git
 git clone https://github.com/5GinFIRE/eu.5ginFIRE.riftioyangschema2java.git
+cd eu.5ginFIRE.riftioyangschema2java
+mvn clean install
+cd $dirlocation
 git clone https://github.com/5GinFIRE/nfv-requirements-extractor.git
+cd nfv-requirements-extractor
+mvn clean install
+cd $dirlocation
+cd eu.5ginfire.portal.api
+

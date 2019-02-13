fd07cc21804e9f23f7720accc8a57ca52983263a
Christos Tranoris <tranoris@gmail.com>
2019-02-13 15:50:08 +0200

    add some log messages

--- src/main/java/portal/api/bugzilla/BugzillaClient.java
+++ src/main/java/portal/api/bugzilla/BugzillaClient.java
@@ -256,6 +256,7 @@ public class BugzillaClient {
 
 	public static Bug transformVxFValidation2BugBody(VxFMetadata vxf) {
 
+		logger.info( "In transformVxFValidation2BugBody: alias = " + vxf.getUuid());
 		String product = "5GinFIRE Operations";
 		String component = "Validation" ;
 		String summary = "[PORTAL] Validation Request for VxF:" + vxf.getName() + ", Owner: " + vxf.getOwner().getUsername();
@@ -307,6 +308,8 @@ public class BugzillaClient {
 	}
 	
 	public static Bug transformVxFAutomaticOnBoarding2BugBody(VxFOnBoardedDescriptor vxfobd) {
+		
+		logger.info( "In transformVxFAutomaticOnBoarding2BugBody: alias = " + vxfobd.getUuid());
 
 		String product = "5GinFIRE Operations";
 		String component = "Onboarding" ;
@@ -361,6 +364,7 @@ public class BugzillaClient {
 		
 	
 	public static Bug transformNSDValidation2BugBody(ExperimentMetadata nsd) {
+		logger.info( "In transformNSDValidation2BugBody: alias = " + nsd.getUuid());
 
 		String product = "5GinFIRE Operations";
 		String component = "Validation" ;
@@ -406,6 +410,8 @@ public class BugzillaClient {
 	
 	public static Bug transformNSDAutomaticOnBoarding2BugBody(ExperimentOnBoardDescriptor uexpobd) {
 
+		logger.info( "In transformNSDAutomaticOnBoarding2BugBody: alias = " + uexpobd.getUuid());
+		
 		String product = "5GinFIRE Operations";
 		String component = "Onboarding" ;
 		String summary = "[PORTAL] OSM OnBoarding Action for NSD:" + uexpobd.getExperiment().getName() + ", Owner: " + uexpobd.getExperiment().getOwner().getUsername();
